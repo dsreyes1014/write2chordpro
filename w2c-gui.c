60de12c18cb8ce7ca75ff5c647baf9a165f024f5
Andrew <dsreyes1014@gmail.com>
2013-11-15 19:16:14 -0600

    updated w2c-gui.c

--- w2c-gui.c
+++ w2c-gui.c
@@ -1,5 +1,6 @@
 #include <gtk/gtk.h>
 #include <string.h>
+#include <stdio.h>
 
 GtkWidget *dialog;
 GtkWidget *window;
@@ -7,9 +8,9 @@ GtkWidget *entryTitle, *entryArtist;
 GtkWidget *textView;
 GtkTextBuffer *buffer;
 
-static const char *titles[] = {"Open", "Quit"};
+static const char *fileMenuTitles[] = {"Open", "Save", "Quit"};
 
-static void menuCallBack(GtkMenuItem *item, gpointer data)
+static void fileMenuCallBack(GtkMenuItem *item, gpointer data)
 {
 	char option[20];
 	
@@ -25,7 +26,7 @@ static void menuCallBack(GtkMenuItem *item, gpointer data)
                                              ("_Cancel"), GTK_RESPONSE_CANCEL,
                                              ("_Open"), GTK_RESPONSE_ACCEPT,
                                              NULL);
-		gtk_widget_set_size_request(dialog, 800, 600);
+		gtk_widget_set_size_request(dialog, 100, 100);
 		
 		gtk_widget_show_all(dialog);
 	
@@ -44,9 +45,22 @@ static void menuCallBack(GtkMenuItem *item, gpointer data)
 	}
 }
 
+static void insertChord(GtkWidget *widget, gpointer data)
+{	
+	GtkTextMark *cursor;	
+	GtkTextIter iter;
+	gint moveCursor = gtk_text_iter_backward_chars(&iter, 1);	
+
+	cursor = gtk_text_buffer_get_insert(GTK_TEXT_BUFFER(buffer));
+	gtk_text_iter_backward_chars(GTK_TEXT_ITER(iter), 1);
+	
+	gtk_text_buffer_insert_at_cursor(GTK_TEXT_BUFFER(buffer), "[]", 2);
+	gtk_text_buffer_place_cursor(GTK_TEXT_BUFFER(buffer), iter);
+}
+
 int main(int argc, char *argv[])
 {
-	GtkWidget *box1, *box2;
+	GtkWidget *box1, *box2, *box3;
 	GtkWidget *paned;
 	GtkWidget *frame1, *frame2;
 	GtkWidget *notebook;
@@ -56,11 +70,14 @@ int main(int argc, char *argv[])
 	GtkWidget *fileMenu;
 	GtkWidget *menuItemFile;
 	GtkWidget *tabLabel;
+	GtkWidget *scrolledWindow;
+	GtkWidget *button1, *button2;
 		
 	gtk_init(&argc, &argv);
 	
 	box1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
 	box2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
+	box3 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
 	paned = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
 	menuBar = gtk_menu_bar_new();
 	fileMenu = gtk_menu_new();
@@ -71,10 +88,14 @@ int main(int argc, char *argv[])
 	label2 = gtk_label_new("Artist:");
 	entryTitle = gtk_entry_new();
 	entryArtist = gtk_entry_new();
-	textView = gtk_text_view_new();
-	buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
 	frame1 = gtk_frame_new(NULL);	
 	frame2 = gtk_frame_new(NULL);
+	scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
+	button1 = gtk_button_new_with_label("Insert Chord");
+	button2 = gtk_button_new_with_label("Transpose");
+	textView = gtk_text_view_new();	
+	buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
+	
 	
 	/* This creates main window titled 'Write 2 Chordpro'. */	
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -85,34 +106,49 @@ int main(int argc, char *argv[])
 	
 	gtk_menu_bar_set_pack_direction(GTK_MENU_BAR(menuBar), GTK_PACK_DIRECTION_LTR);
 	
-	/* Creates menubar */
-	for(int i = 0; i < 2; i++)
+	/* Creates menuitem 'File' and its menu for menuBar */
+	for(int i = 0; i < 3; i++)
 	{
-		GtkWidget *menuItem = gtk_menu_item_new_with_label(titles[i]);
+		GtkWidget *menuItem = gtk_menu_item_new_with_label(fileMenuTitles[i]);
 		
 		gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), menuItem);
-		g_signal_connect(menuItem, "activate", G_CALLBACK(menuCallBack), NULL);
+		g_signal_connect(menuItem, "activate", G_CALLBACK(fileMenuCallBack), NULL);
 	}
 		
 	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menuItemFile), fileMenu);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menuBar), menuItemFile);
 		
-	/* Packs menubar and paned in boxes 1 and 2 respectively of box1 */
+	/* Packs menubar and paned in box1 */
 	gtk_box_pack_start(GTK_BOX(box1), menuBar, FALSE, FALSE, 2);
 	gtk_box_pack_start(GTK_BOX(box1), paned, FALSE, FALSE, 2);
 	
 	gtk_frame_set_shadow_type(GTK_FRAME(frame1), GTK_SHADOW_IN);
 	gtk_frame_set_shadow_type(GTK_FRAME(frame2), GTK_SHADOW_ETCHED_IN);
+
+	/* Sizes frame2 */	
+	gtk_widget_set_size_request(GTK_WIDGET(frame2), 100, 100);
 	
-	/* Adds textView inside of frame2 */
-	gtk_container_add(GTK_CONTAINER(frame2), textView);
+	/* Packs textView inside of scrolledWindow */	
+	gtk_container_add(GTK_CONTAINER(scrolledWindow), textView);
 	
-	/* Attaches labels to grid */	
+	/* Packs scrolledWindow into frame2 */
+	gtk_container_add(GTK_CONTAINER(frame2), scrolledWindow);
+	
+	//gtk_button_set_alignment(GTK_BUTTON(button1), 0.5, 0.2);
+	
+	/* Places button1 & button2 into box3 */
+	gtk_box_pack_start(GTK_BOX(box3), button1, FALSE, FALSE, 2);
+	gtk_box_pack_start(GTK_BOX(box3), button2, FALSE, FALSE, 2);
+	
+	g_signal_connect(button1, "clicked", G_CALLBACK(insertChord), NULL);
+		
+	/* Attaches widgets to widget grid */	
 	gtk_grid_attach(GTK_GRID(grid), label1, 0, 0, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), label2, 0, 10, 1, 1);	
 	gtk_grid_attach_next_to(GTK_GRID(grid), entryTitle, label1, GTK_POS_RIGHT, 1, 1);
 	gtk_grid_attach_next_to(GTK_GRID(grid), entryArtist, label2, GTK_POS_RIGHT, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), frame2, 0, 14, 130, 150);
+	gtk_grid_attach_next_to(GTK_GRID(grid), box3, frame2, GTK_POS_RIGHT, 1, 1);
 	
 	/* Sets row & column spacing between widgets inside grid */
 	gtk_grid_set_row_spacing(GTK_GRID(grid), 3);
@@ -128,13 +164,13 @@ int main(int argc, char *argv[])
 	tabLabel = gtk_label_new("Song Editor");	
 	gtk_notebook_set_tab_label(GTK_NOTEBOOK(notebook), grid, tabLabel);
 	
-	/* Packs frame 1 & notebook widgets inside panes 1 & 2 respectively */	
+	/* Packs frame1 & notebook widgets inside panes 1 & 2 respectively */	
 	gtk_paned_pack1(GTK_PANED(paned), frame1, FALSE, FALSE);
 	gtk_paned_pack2(GTK_PANED(paned), notebook, FALSE, FALSE);
 	
 	/* Sizes both frame widgets */ 
-	gtk_widget_set_size_request(frame1, 20, 150);
-	gtk_widget_set_size_request(notebook, 50, 150);
+	gtk_widget_set_size_request(frame1, 200, 75);
+	gtk_widget_set_size_request(notebook, 300, 150);
 	
 	/* This signal terminates main window when the close button is clicked */	
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
