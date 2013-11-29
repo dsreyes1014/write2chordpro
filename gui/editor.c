b8354a29238b2b7debf73bf95104148a7c2eae18
drew <dsreyes1014@gmail.com>
2013-11-29 17:50:02 -0600

    updated the gui source code

--- gui/editor.c
+++ gui/editor.c
@@ -8,13 +8,15 @@
 #include "songs.h"
 
 GtkWidget *entryTitle, *entryArtist, *textView, *grid;
+GtkEntryBuffer *entryBuffer;
 GtkTextBuffer *buffer;
 const gchar *getTitle, *getArtist;
 extern char directory[];
 extern GtkListStore *listStore;
+extern GtkTreeSelection *selection;
 
 void save(GtkWidget *widget, gpointer data)
-{
+{	
 	GtkTextIter start, end;	
 	gchar *body;
 	char newFile[25];
@@ -32,26 +34,53 @@ void save(GtkWidget *widget, gpointer data)
 	
 	pFile = fopen(newFile, "w+");
 		
-	fprintf(pFile, "\{title: %s\}\n", getTitle);
-	fprintf(pFile, "\{subtitle: %s\}\n\n", getArtist);
-	fprintf(pFile, "%s", body);
+	fprintf(pFile, "{title: %s}\n", getTitle);
+	fprintf(pFile, "{subtitle: %s}\n\n", getArtist);
+	fprintf(pFile, "%s\n", body);
 	
 	fclose(pFile);
 	
 	gtk_list_store_clear(GTK_LIST_STORE(listStore));	
 	
 	listFiles();	
+}
+
+void newSong(GtkWidget *widget, gpointer data)
+{
+	GtkTextIter start, end;	
 	
-	g_print("Title is: %s\n", getTitle);
-	g_print("Artist is: %s\n", getArtist);	
-	g_print("%s\n", body);
+	// Unselects all in 'songList' so it will not appear in song editor.	
+	gtk_tree_selection_unselect_all(GTK_TREE_SELECTION(selection));	
+	
+	entryBuffer = gtk_entry_get_buffer(GTK_ENTRY(entryTitle));
+	
+	gtk_entry_buffer_delete_text(GTK_ENTRY_BUFFER(entryBuffer), 0, -1);
+
+	entryBuffer = gtk_entry_get_buffer(GTK_ENTRY(entryArtist));
+	
+	gtk_entry_buffer_delete_text(GTK_ENTRY_BUFFER(entryBuffer), 0, -1);
+	
+	gtk_text_buffer_get_start_iter(GTK_TEXT_BUFFER(buffer), &start);
+	gtk_text_buffer_get_end_iter(GTK_TEXT_BUFFER(buffer), &end);
+	
+	gtk_text_buffer_delete(GTK_TEXT_BUFFER(buffer), &start, &end);
+}
+
+void editSong(GtkWidget *widget, gpointer data)
+{
+	gtk_editable_set_editable(GTK_EDITABLE(entryTitle), TRUE);
+	
+	gtk_editable_set_editable(GTK_EDITABLE(entryArtist), TRUE);
+	
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(textView), TRUE);
 }
 
 void editor(void)
 {
 	GtkWidget *label1, *label2, *button1, *button2, *button3,
-			  *frame, *box, *scrolledWindow;
-	
+			    *button4, *button5, *frame, *boxTop, *scrolledWindow,
+			    *boxBottom;
+		
 	grid = gtk_grid_new();
 	entryTitle = gtk_entry_new();
 	entryArtist = gtk_entry_new();
@@ -61,9 +90,12 @@ void editor(void)
 	buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
 	button1 = gtk_button_new_with_label("Insert Chord");
 	button2 = gtk_button_new_with_label("Transpose");
-	button3 = gtk_button_new_with_label("Add");
+	button3 = gtk_button_new_with_label("Add New");
+	button4 = gtk_button_new_with_label("Save");
+	button5 = gtk_toggle_button_new_with_label("Edit");
 	frame = gtk_frame_new(NULL);
-	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
+	boxTop = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
+	boxBottom = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
 	scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
 	
 	gtk_entry_set_placeholder_text(GTK_ENTRY(entryTitle), "Enter name of title here");
@@ -75,10 +107,16 @@ void editor(void)
 	// Properties for 'frame' widget.	
 	gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_ETCHED_IN);	
 	gtk_widget_set_size_request(GTK_WIDGET(frame), 100, 100);
-
-	// Packs 'button1' & 'button2' widgets into 'box' widget.
-	gtk_box_pack_start(GTK_BOX(box), button1, FALSE, FALSE, 2);
-	gtk_box_pack_start(GTK_BOX(box), button2, FALSE, FALSE, 2);    
+	
+	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(button5), FALSE);
+	
+	// Packs 'button3' & 'button4' into 'boxTop'.
+	gtk_box_pack_start(GTK_BOX(boxTop), button3, FALSE, FALSE, 2);
+	gtk_box_pack_start(GTK_BOX(boxTop), button5, FALSE, FALSE, 2);
+	gtk_box_pack_start(GTK_BOX(boxTop), button4, FALSE, FALSE, 2); 
+	// Packs 'button1' & 'button2' widgets into 'boxBottom' widget.
+	gtk_box_pack_start(GTK_BOX(boxBottom), button1, FALSE, FALSE, 2);
+	gtk_box_pack_start(GTK_BOX(boxBottom), button2, FALSE, FALSE, 2);    
 	
 	// Packs 'textView' widget inside of 'scrolledWindow' widget.	
 	gtk_container_add(GTK_CONTAINER(scrolledWindow), textView);
@@ -89,11 +127,12 @@ void editor(void)
     // Attaches widgets to widget grid.	
 	gtk_grid_attach(GTK_GRID(grid), label1, 3, 1, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), label2, 3, 10, 1, 1);	
-	gtk_grid_attach_next_to(GTK_GRID(grid), entryTitle, label1, GTK_POS_RIGHT, 100, 1);
-	gtk_grid_attach_next_to(GTK_GRID(grid), entryArtist, label2, GTK_POS_RIGHT, 100, 1);
+	gtk_grid_attach_next_to(GTK_GRID(grid), entryTitle, label1, GTK_POS_RIGHT, 80, 1);
+	gtk_grid_attach_next_to(GTK_GRID(grid), entryArtist, label2, GTK_POS_RIGHT, 80, 1);
 	gtk_grid_attach(GTK_GRID(grid), frame, 0, 14, 130, 150);
-	gtk_grid_attach_next_to(GTK_GRID(grid), box, frame, GTK_POS_RIGHT, 1, 1);
-	gtk_grid_attach_next_to(GTK_GRID(grid), button3, entryArtist, GTK_POS_RIGHT, 1, 1);
+	gtk_grid_attach_next_to(GTK_GRID(grid), boxTop, frame, GTK_POS_RIGHT, 1, 50);
+	gtk_grid_attach_next_to(GTK_GRID(grid), boxBottom, boxTop, GTK_POS_BOTTOM, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), button3, 80, 10, 1, 1);
 	
 	// Sets row & column spacing between widgets inside 'grid' widget.
 	gtk_grid_set_row_spacing(GTK_GRID(grid), 3);
@@ -102,5 +141,7 @@ void editor(void)
 	// Connects function 'insertChord' to 'button1' widget.	
 	g_signal_connect(button1, "clicked", G_CALLBACK(insertChord), NULL);
 	// Connects function 'save' to 'button3' labeled "Save".
-	g_signal_connect(button3, "clicked", G_CALLBACK(save), NULL);
+	g_signal_connect(button4, "clicked", G_CALLBACK(save), NULL);
+	g_signal_connect(button3, "clicked", G_CALLBACK(newSong), NULL);
+	g_signal_connect(button5, "toggled", G_CALLBACK(editSong), NULL);
 }
